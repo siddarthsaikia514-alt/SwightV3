e09d428d1a2a7239ceed1212a5fdeffe792aca81
siddarthsaikia514-alt <siddarthsaikia514@gmail.com>
2026-04-02 22:43:31 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -136,7 +136,7 @@ export const botConfig = {
     },
     footer: {
       // Default footer text used in bot embeds.
-      text: "Titan Bot",
+      text: "Archnemesis",
       // Footer icon URL (null = no icon).
       icon: null,
     },
